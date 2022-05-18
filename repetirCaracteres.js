b5408f9edd5e3398cd22754af26011560a8b589d
sulbaranhuamani <sulbaranhuamani@gmail.com>
2022-05-17 22:33:05 -0500

    nex two

--- repetirCaracteres.js
+++ repetirCaracteres.js
@@ -1,8 +1,10 @@
 function repetirCaracteres(string){
+    //separando las palabras
     let arraySplit = string.split(' ');
     let narray = new Array();
+    //recorriendo las palabras y copiando en el nuevo array.
     for(var i = 0; i < arraySplit.length; i++){
-        narray[i] = arraySplit[i] + ' ' + arraySplit[i]
+        narray[i] = arraySplit[i] + arraySplit[i]
     }
     return narray.join(' ');
 }
