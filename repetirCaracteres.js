cdcb002c6fa0ed4d556715011b7f793319916f58
sulbaranhuamani <sulbaranhuamani@gmail.com>
2022-05-17 22:29:58 -0500

    nex one

--- repetirCaracteres.js
+++ repetirCaracteres.js
@@ -1,3 +1,11 @@
+function repetirCaracteres(string){
+    let arraySplit = string.split(' ');
+    let narray = new Array();
+    for(var i = 0; i < arraySplit.length; i++){
+        narray[i] = arraySplit[i] + ' ' + arraySplit[i]
+    }
+    return narray.join(' ');
+}
 with(document){
     write('hello')
 }
